5e589f3b41d6da8a617c98950d2aee43541c2901
MAD-G <madhav.subramanyam@gmail.com>
2016-12-04 15:50:02 -0500

    Merchants and hostiles AI

---
+++ controller/hostile.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Controller.h"
+
+struct HostileCharacter : public Controller
+{
+	const ActionAndTarget getAction(const std::shared_ptr<Character> &character, const std::vector<ActionAndRange> &actions,
+                                            const std::shared_ptr<State> &state);
+};

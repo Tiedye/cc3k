5fd3ec1bf021c1b12b50a1b07b7c144d4587f24a
ADBOURQU <ADBOURQU@uWaterloo.ca>
2016-12-04 20:34:38 -0500

    menu

--- stage/Menu.cc
+++ stage/Menu.cc
@@ -1,10 +1,36 @@
+#include <string>
+#include <iostream>
 #include "Menu.h"
 #include "../Game.h"
 
 using namespace std;
 
 int Menu::run(Game &game) {
-    state->player->addFeatureSet(*(*state->library.getRaces().begin())->featureSet);
+    string cmd;
+	cout << "Please select a class. For a list of classes, type: list. for info on each class type: stats 'classname'." << endl;
+	while(cin >> cmd){
+		if(cmd == "list" || cmd == "List"){
+			for(auto& pair:state->library.getRaces()) {
+				cout << "(" << pair->shortcut <<") "<< pair.second->name <<", ";
+			}
+			cout << "(Rand) Random" << endl;
+		} else if (cmd == "stats"|| cmd =="Stats"){
+			cin >> cmd;
+			for(auto& pair:state->library.getRaces()) {
+				if(pair.name == cmd  || pair.shortcut == cmd){
+					cout << pair.description << endl;
+					break;
+				}
+			}
+		} else {
+			for(auto& pair:state->library.getRaces()) {
+				if(pair.name == cmd  || pair.shortcut == cmd){
+					state->player->addFeatureSet(*state->library.getRaceSet(state->library.getRaces().begin()->first));
+					break;
+				}
+			}
+		}
+	}
     return next;
 }
 

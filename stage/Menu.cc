e096a8520c48f7c0fa9cc0194c0e8e3f08011a8f
Alexander Douglas Bourque <adbourque@uwaterloo.ca>
2016-12-05 09:42:21 -0500

    Revert "idk"
    This reverts commit cab59fb4503e14d49e3341f9d9af9f1babc21a5c

--- stage/Menu.cc
+++ stage/Menu.cc
@@ -7,14 +7,14 @@ using namespace std;
 
 int Menu::run(Game &game) {
     string cmd;
-	cout << "Please select a class. For a list of classes, type: list. for info on each class type: desc 'classname'." << endl;
+	cout << "Please select a class. For a list of classes, type: list. for info on each class type: stats 'classname'." << endl;
 	while(cin >> cmd){
 		if(cmd == "list" || cmd == "List"){
 			for(auto& pair:state->library.getRaces()) {
-				cout << "(" << pair->shortcut <<") "<< pair->name <<" ";
+				cout << "(" << pair->shortcut <<") "<< pair->name <<", ";
 			}
-			cout << endl;
-		} else if (cmd == "desc"|| cmd =="Desc"){
+			cout << "(Rand) Random" << endl;
+		} else if (cmd == "stats"|| cmd =="Stats"){
 			cin >> cmd;
 			for(auto& pair:state->library.getRaces()) {
 				if(pair->name == cmd  || pair->shortcut == cmd){
@@ -22,13 +22,10 @@ int Menu::run(Game &game) {
 					break;
 				}
 			}
-<<<<<<< Updated upstream
 		} else if (cmd == "Rand" || cmd == "Random"){
 			state->player->addFeatureSet(*state->library.getRaces()[uniform_int_distribution<>(0, state->library.getRaces().size()-1)(state->gen)]->featureSet);
 			state->player->create();
 			return next;
-=======
->>>>>>> Stashed changes
 		} else {
 			for(auto& pair:state->library.getRaces()) {
 				if(pair->name == cmd  || pair->shortcut == cmd){
